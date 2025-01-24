1d420047db6bd36a33b2ec0a4b152c87529b3a49
unknown <nicolasmonteiro0123@gmail.com>
2025-01-24 20:01:51 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,11 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://atlasdesenvolvimento.vercel.app', // Permitir apenas esse domínio
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    credentials: true, // Permite envio de cookies, se necessário
+  }));
 
 app.use(bodyParser.json());
 
