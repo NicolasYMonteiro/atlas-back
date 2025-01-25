cf80362649fe047c39bfbffa94a702b00a80ff5d
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-01-25 09:28:43 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const routerUser = require('./Routes/routerUser');
 const routerTask = require('./Routes/routerTask');
 const routerSubTask = require('./Routes/routerSubTask')
 
-const cors = require('cors');
 const app = express();
 
 app.use(cors({
     origin: 'https://atlasdevelopment.vercel.app', // Permitir apenas esse domínio
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
-  }));
+}));
+
+app.options('*', cors()); // Responde automaticamente às preflight requests
 
 app.use(bodyParser.json());
 
