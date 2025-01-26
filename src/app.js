e13d113f934255aa5439272da56d443bbeef20b1
NicolasYMonteiro <nicolasmonteiro0123@gmail.com>
2025-01-26 10:02:21 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -9,14 +9,14 @@ const routerSubTask = require('./Routes/routerSubTask')
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:4200', // Permitir apenas esse domínio
+    origin: '*', // Permitir apenas esse domínio
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
 }));
 
 // Middleware para lidar com solicitações preflight (OPTIONS)
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
